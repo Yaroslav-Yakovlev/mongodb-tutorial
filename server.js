a4d2dce28762b2fb9fc649be4130fafef30402e8
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2024-07-06 19:18:26 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const movieRoutes = require('./routes/movie-routes');
+
+const PORT = 3000;
+const URL = "mongodb://localhost:27017/moviebox";
+
+const app = express();
+app.use(express.json());
+app.use(movieRoutes);
+
+mongoose.
+  connect(URL, { useNewUrlParser: true, useUnifiedTopology : true }).
+  then(() => console.log('Connected to MongoDB')).
+  catch((err) => console.log(`DB connection error: ${err}`));
+
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log(`Lisening port ${PORT}`);
+});
+
